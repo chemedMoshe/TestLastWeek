143fb88a08f30e1ec8ce68dbd32df64c02f10104
mosheChemed <limudimIDF@gmail.com>
2024-12-24 17:12:28 +0200

    add crad by socket

--- ServerSide/src/Socket/Socket.ts
+++ ServerSide/src/Socket/Socket.ts
@@ -1,3 +1,4 @@
+import CasesModel from "../DBModel/CasesModel";
 import { io } from "../index";
 import { getAllTypeAttack } from "../Server/AttacktypeServer";
 import { getAllCantry } from "../Server/CantryServer";
@@ -24,4 +25,36 @@ socket.on("getAllOrg", async(year) =>{
     
     socket.emit("sendOrg", org);
 } )
-}) 
+// Create a new case
+    socket.on("createCase", async (data) => {
+        try {
+            const newCase = new CasesModel(data);
+            const savedCase = await newCase.save();
+            socket.emit("caseCreated", savedCase); 
+        } catch (err) {
+            socket.emit("error", { message: "Failed to create case", error: err });
+        }
+    });
+
+    // Update an existing case
+        socket.on("updateCase", async ({ id, updates }) => {
+            try {
+                const updatedCase = await CasesModel.findByIdAndUpdate(id, updates, { new: true });
+                socket.emit("caseUpdated", updatedCase); 
+            } catch (err) {
+                socket.emit("error", { message: "Failed to update case", error: err });
+            }
+        });
+        // Delete a case
+        socket.on("deleteCase", async (id) => {
+            try {
+                await CasesModel.findByIdAndDelete(id);
+                socket.emit("caseDeleted", { id }); // מאשר את המחיקה ללקוח
+            } catch (err) {
+                socket.emit("error", { message: "Failed to delete case", error: err });
+            }
+        });
+    });
+    
+
+
